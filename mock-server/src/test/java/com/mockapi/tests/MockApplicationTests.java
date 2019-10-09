08f83283b51acceb711d7af1f90285cc69996a66
Kunwardeep Singh <kunwardeep.singh@gmail.com>
2019-10-09 13:42:33 -0400

    added unit test

---
+++ mock-server/src/test/java/com/mockapi/tests/MockApplicationTests.java
@@ -0,0 +1,34 @@
+package com.mockapi.tests;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MockApplicationTests {
+	
+	@Autowired
+	private WebApplicationContext webApplicationContext;
+
+	private MockMvc mockMvc;
+
+	@Before
+	public void setup() {
+		mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+	}
+
+	@Test
+	public void testGet() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.get("/mock/5d9e107bf31e5d0cd8adb88b")).andExpect(MockMvcResultMatchers.status().isOk());
+	}
+
+}
